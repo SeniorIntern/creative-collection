1426bc066772c995a689c0e3c908f6f6ff178f2f
Nikhil Thapa <tnikhil.official@gmail.com>
2022-11-07 14:06:42 +0545

    css files for components.

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,9 @@ import React from "react";
 export default function About() {
     return (
         <div className="about__container">
-            <div className="about__work"></div>
+            <div className="about__work">
+                
+            </div>
             <div className="about_ceo"></div>
             <div className="about_proudteammates"></div>
         </div>
