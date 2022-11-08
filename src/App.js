b083849d8608de76708876bdeb1458ab24d67256
Roshan Sadha Sanker <70261891+RoshanBro77@users.noreply.github.com>
2022-11-08 17:21:27 +0545

    Header Color and adjusted looked by plaing with font sizes and set width to 80 %

--- src/App.js
+++ src/App.js
@@ -1,32 +1,33 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./assets/styles/App.css";
-import About from "./components/About";
-import { BusinessVerticles } from "./components/BusinessVerticles";
-import Contact from "./components/Contact";
-import Events from "./components/Events";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import HomeBody from "./components/HomeBody";
-import NotFound from "./components/NotFound";
-import Products from "./components/Products";
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import './assets/styles/App.css'
+import About from './components/About'
+import { BusinessVerticles } from './components/BusinessVerticles'
+import Contact from './components/Contact'
+import Events from './components/Events'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import HomeBody from './components/HomeBody'
+import NotFound from './components/NotFound'
+import Products from './components/Products'
 
 export default function App() {
   return (
-        <Router>
-            <div className="app__container">
-                <Header />
-                <Routes>
-                    {/* Handle path exception. root path ="/" */}
-                    <Route path="/" element={<HomeBody />} />
-                    <Route path="/products" element={<Products />} />
-                    <Route path="/about" element={<About />} />
-                    <Route path="/events" element={<Events />} />
-                    <Route path="/contact" element={<Contact />} />
-                    <Route path="*" element={<NotFound />} />
-                </Routes>
-            </div>
-            <Footer />
-        </Router>
-    );
+    <Router>
+      <div className='mainBox'>
+        <Header />
+        <div className='app__container'>
+          <Routes>
+            <Route path='/' element={<HomeBody />} />
+            <Route path='/products' element={<Products />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/events' element={<Events />} />
+            <Route path='/contact' element={<Contact />} />
+            <Route path='*' element={<NotFound />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </Router>
+  )
 }
--- src/components/HomeBody.js
+++ src/components/HomeBody.js
@@ -1,60 +1,56 @@
-import React from "react";
-import "../assets/styles/HomeBody.css";
-import im1 from "../assets/images/R.jfif";
-import im2 from "../assets/images/OIP (1).jfif";
-import im3 from "../assets/images/OIP.jfif";
-import HeroContainer from "./HeroContainer";
+import React from 'react'
+import '../assets/styles/HomeBody.css'
+import im1 from '../assets/images/R.jfif'
+import im2 from '../assets/images/OIP (1).jfif'
+import im3 from '../assets/images/OIP.jfif'
+import HeroContainer from './HeroContainer'
 
 export default function HomeBody() {
-    return (
-        <div className="homeBody">
-            <div className="homeBody__container">
-                <HeroContainer />
-                <div className="gradient__Container" />
+  return (
+    <div className='homeBody'>
+      <div className='homeBody__container'>
+        <HeroContainer />
+        <div className='gradient__Container' />
+      </div>
+      <h4>Latest Events</h4>
+      <div className='container'>
+        <div className='left__homebody'>
+          <div className='left__homebody__upperContainer'>
+            <div className='container__uppercontainer__image'>
+              <img className='homeBodyImg' src={im1} alt='' />
+              <div className='bottom__left'>Here is something</div>
             </div>
-            <h4>Latest Events</h4>
-            <div className="container">
-                <div className="left__homebody">
-                    <div className="left__homebody__upperContainer">
-                        <div className="container__uppercontainer__image">
-                            <img className="homeBodyImg" src={im1} alt="" />
-                            <div className="bottom__left">
-                                Here is something
-                            </div>
-                        </div>
-                    </div>
-                    <div className="left__homebody__lowerContainer">
-                        <div className="left__homebody__lowerContainer__containers">
-                            <img src={im2} className="homeBodyImg" alt="" />
-                        </div>
-                        <div className="left__homebody__lowerContainer__containers">
-                            <img src={im3} className="homeBodyImg" alt="" />
-                        </div>
-                    </div>
-                </div>
-                <div className="right__homebody">
-                    <div className="right__homebody__container">
-                        <h5> What's hot</h5>
-
-                        <div className="right__homebody__component">
-                            <img src={im1} alt="" />
-                            <p className="homebody__time">2 hrs ago</p>
-                            <p className="homebodycontents">
-                                Lorem ipsum dolor sit amet consectetur
-                                adipisicing elit.
-                            </p>
-                        </div>
-                        <div className="right__homebody__component">
-                            <img src={im1} alt="" />
-                            <p className="homebody__time">2 hrs ago</p>
-                            <p className="homebodycontents">
-                                Lorem ipsum dolor sit amet consectetur
-                                adipisicing elit.
-                            </p>
-                        </div>
-                    </div>
-                </div>
+          </div>
+          <div className='left__homebody__lowerContainer'>
+            <div className='left__homebody__lowerContainer__containers'>
+              <img src={im2} className='homeBodyImg' alt='' />
             </div>
+            <div className='left__homebody__lowerContainer__containers'>
+              <img src={im3} className='homeBodyImg' alt='' />
+            </div>
+          </div>
         </div>
-    );
+        <div className='right__homebody'>
+          <div className='right__homebody__container'>
+            <h5> What's hot</h5>
+
+            <div className='right__homebody__component'>
+              <img src={im1} alt='' />
+              <p className='homebody__time'>2 hrs ago</p>
+              <p className='homebodycontents'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </p>
+            </div>
+            <div className='right__homebody__component'>
+              <img src={im1} alt='' />
+              <p className='homebody__time'>2 hrs ago</p>
+              <p className='homebodycontents'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
