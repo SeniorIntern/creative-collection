5feae12d1a6b2c858ac70adc0d5d95ebedea37f2
Nikhil Thapa <tnikhil.official@gmail.com>
2022-11-09 12:36:55 +0545

    Iteration 03. Working on Events-Event component.

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,9 +1,35 @@
 import React from "react";
+import Event from "./Event";
+import event1 from "../assets/images/Event1.jpg";
+import event2 from "../assets/images/Event2.jpg";
+import event3 from "../assets/images/Event3.jpg";
+import "../assets/styles/Events.css";
 
 export default function Events() {
     return (
         <div className="events">
-            <h1>This is Events page</h1>
+            <h1 className="eventTitle">
+                Welcome to Our Events Archieve
+            </h1>
+            <div className="events__list">
+                <Event
+                    id="1"
+                    title="Celebrating Annual Anniversary 2022"
+                    image={event1}
+                />
+
+                <Event
+                    id="2"
+                    title="Happy Thanksgiving Program 2022"
+                    image={event2}
+                />
+
+                <Event
+                    id="3"
+                    title="Holly Celebration by Creative Group - 2022"
+                    image={event3}
+                />
+            </div>
         </div>
     );
 }
--- src/components/HomeBody.js
+++ src/components/HomeBody.js
@@ -1,56 +1,62 @@
-import React from 'react'
-import '../assets/styles/HomeBody.css'
-import im1 from '../assets/images/R.jfif'
-import im2 from '../assets/images/OIP (1).jfif'
-import im3 from '../assets/images/OIP.jfif'
-import HeroContainer from './HeroContainer'
+import React from "react";
+import "../assets/styles/HomeBody.css";
+import event1 from "../assets/images/Event1.jpg";
+import event2 from "../assets/images/Event2.jpg";
+import event3 from "../assets/images/Event3.jpg";
+import event4 from "../assets/images/Event4.jpg";
+import event5 from "../assets/images/Event5.jpg";
+import HeroContainer from "./HeroContainer";
 
 export default function HomeBody() {
-  return (
-    <div className='homeBody'>
-      <div className='homeBody__container'>
-        <HeroContainer />
-        <div className='gradient__Container' />
-      </div>
-      <h4>Latest Events</h4>
-      <div className='container'>
-        <div className='left__homebody'>
-          <div className='left__homebody__upperContainer'>
-            <div className='container__uppercontainer__image'>
-              <img className='homeBodyImg' src={im1} alt='' />
-              <div className='bottom__left'>Here is something</div>
+    return (
+        <div className="homeBody">
+            <div className="homeBody__container">
+                <HeroContainer />
+                <div className="gradient__Container" />
             </div>
-          </div>
-          <div className='left__homebody__lowerContainer'>
-            <div className='left__homebody__lowerContainer__containers'>
-              <img src={im2} className='homeBodyImg' alt='' />
-            </div>
-            <div className='left__homebody__lowerContainer__containers'>
-              <img src={im3} className='homeBodyImg' alt='' />
-            </div>
-          </div>
-        </div>
-        <div className='right__homebody'>
-          <div className='right__homebody__container'>
-            <h5> What's hot</h5>
+            <h4>Latest Events</h4>
+            <div className="container">
+                <div className="left__homebody">
+                    <div className="left__homebody__upperContainer">
+                        <div className="container__uppercontainer__image">
+                            <img className="homeBodyImg" src={event1} alt="" />
+                            <div className="bottom__left">
+                                Celebrating annual anniversary
+                            </div>
+                        </div>
+                    </div>
+                    <div className="left__homebody__lowerContainer">
+                        <div className="left__homebody__lowerContainer__containers">
+                            <img src={event2} className="homeBodyImg" alt="" />
+                        </div>
+                        <div className="left__homebody__lowerContainer__containers">
+                            <img src={event3} className="homeBodyImg" alt="" />
+                        </div>
+                    </div>
+                </div>
+                <div className="right__homebody">
+                    <div className="right__homebody__container">
+                        <h5> What's hot</h5>
 
-            <div className='right__homebody__component'>
-              <img src={im1} alt='' />
-              <p className='homebody__time'>2 hrs ago</p>
-              <p className='homebodycontents'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              </p>
+                        <div className="right__homebody__component">
+                            <img src={event4} alt="" />
+                            <p className="homebody__time">2 hrs ago</p>
+                            <p className="homebodycontents">
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing elit.
+                            </p>
+                        </div>
+                        <div className="right__homebody__component">
+                            <img src={event5} alt="" />
+                            <p className="homebody__time">2 hrs ago</p>
+                            <p className="homebodycontents">
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing elit.
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className='right__homebody__component'>
-              <img src={im1} alt='' />
-              <p className='homebody__time'>2 hrs ago</p>
-              <p className='homebodycontents'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              </p>
-            </div>
-          </div>
         </div>
-      </div>
-    </div>
-  )
+    );
 }
